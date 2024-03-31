cb6a3f9058ae3fd7d99c335f81593dfd72d5ecd7
ABERsara <sara0556799675@gmail.com>
2024-04-01 00:50:03 +0300

    change on data file from branch2

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 games=[{
-    id:112,
+    id:312,
     name:"Bool Pgiah",
     price:151.2,
     image:"c2",
     discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},{id:212,
+},{id:314,
     name:"Hangman Game",
     price:280,
     image:"Ciro el pirata.jpg",
